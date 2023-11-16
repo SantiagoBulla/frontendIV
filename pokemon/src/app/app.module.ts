e4e3d776e06e4ad7e4d89a1db08b7a43566cb245
Santiago Bulla <santiagobulla1216@gmail.com>
2023-11-16 16:50:29 -0500

    creacion del login

--- pokemon/src/app/app.module.ts
+++ pokemon/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule, 
+    CommonModule //para poder usar el ngClass, que cambia el color segun el estado del formulario
   ],
   providers: [],
   bootstrap: [AppComponent]
