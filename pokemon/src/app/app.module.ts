5603a87acd53cc6de62829f801b4fe82dd3e198c
Santiago Bulla Perez <santiagobulla1216@gmail.com>
2023-11-19 21:50:55 -0500

    cambios

--- pokemon/src/app/app.module.ts
+++ pokemon/src/app/app.module.ts
@@ -8,17 +8,13 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AsideMenuComponent } from './aside-menu/aside-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginFormComponent,
-    RegisterFormComponent,
-    DashboardComponent,
-    AsideMenuComponent
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
