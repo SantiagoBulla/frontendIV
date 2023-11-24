27575de1e5037c735d54d234fd4ca3abf0436c91
Santiago Bulla Perez <santiagobulla1216@gmail.com>
2023-11-24 11:23:06 -0500

    agregar validaciones al login  y al form

--- angularOne/Formularios/src/app/components/login-form/login-form.component.ts
+++ angularOne/Formularios/src/app/components/login-form/login-form.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { ForgotPassModalComponent } from '../forgot-pass-modal/forgot-pass-modal.component';
 
 @Component({
@@ -7,15 +9,29 @@ import { ForgotPassModalComponent } from '../forgot-pass-modal/forgot-pass-modal
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
 
-  constructor(private dialog: MatDialog) {}
+  public loginForm!: FormGroup; 
+
+  constructor(private router: Router, private formBuilder:FormBuilder, private dialog: MatDialog){}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['',[Validators.required]],
+      password: ['',[Validators.required, Validators.minLength(8)]]
+    });
+  }
 
   openForgotPasswordModal() {
     this.dialog.open(ForgotPassModalComponent, {
       width: '600px', // ajusta según tus necesidades
       height: '400px', // ajusta según tus necesidades
     });
+  }
+
+  login(){
+    console.log(this.loginForm.get('username')?.value);
+    console.log(this.loginForm.get('password')?.value);
   }
 
 }
--- angularOne/Formularios/src/app/components/register-form/register-form.component.ts
+++ angularOne/Formularios/src/app/components/register-form/register-form.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.css']
 })
-export class RegisterFormComponent {
+export class RegisterFormComponent implements OnInit {
 
+  
+  public registerForm!: FormGroup; 
+
+  constructor(private router: Router, private formBuilder:FormBuilder){}
+  
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: ['',[Validators.required]],
+      dateOfBirth: ['',[Validators.required]],
+      category: ['', [Validators.required]],
+      password: ['',[Validators.required, Validators.minLength(8)]]
+    });
+  }
+  // Accede fácilmente a los controles del formulario en tu template
+  get f() { return this.registerForm.controls; }
+
+  register(){}
 }
