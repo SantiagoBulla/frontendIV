4289c464e2f2db4f710374b625402da77d615aca
Santiago Bulla Perez <santiagobulla1216@gmail.com>
2023-11-24 14:38:18 -0500

    finalizacion de estilos de la aplicacion

--- angularOne/Formularios/src/app/components/forgot-pass-modal/forgot-pass-modal.component.ts
+++ angularOne/Formularios/src/app/components/forgot-pass-modal/forgot-pass-modal.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-pass-modal',
   templateUrl: './forgot-pass-modal.component.html',
   styleUrls: ['./forgot-pass-modal.component.css']
 })
-export class ForgotPassModalComponent {
+export class ForgotPassModalComponent implements OnInit {
 
-  constructor(private dialogRef: MatDialogRef<ForgotPassModalComponent>) {}
+  public forgotPasswordForm!: FormGroup; 
+
+  constructor(private dialogRef: MatDialogRef<ForgotPassModalComponent>, private router: Router, private formBuilder:FormBuilder) {}
+ 
+  ngOnInit(): void {
+    this.forgotPasswordForm = this.formBuilder.group({
+      username: ['',[Validators.required]],
+      password: ['',[Validators.required, Validators.minLength(8)]]
+    });
+  }
 
   closeModal() {
     // Cierra la modal
     this.dialogRef.close();
+  }
+
+  resetPassword(){
+
   }
 }
